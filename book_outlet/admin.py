c664bad191012daadac19f251aee50f18e5a42ad
mnjammy007 <naseem4954r@gmail.com>
2024-07-31 17:50:22 +0530

    module 7 completed

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "author")
+    list_filter = ("author", "rating")
+    # search_fields = ("title", "rating")
+
+
+admin.site.register(Book, BookAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -12,11 +12,9 @@ class Book(models.Model):
     )
     author = models.CharField(null=True, max_length=100)
     is_bestseller = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    slug = models.SlugField(
+        default="", blank=True, null=False, db_index=True
+    )
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
